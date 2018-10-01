de33de46f1bdee67635f7171733303f5d68687e8
Maria Yala <denise_myala@yahoo.com>
2018-10-01 14:58:46 -0400

    added check for image clicked

--- js/sketch.js
+++ js/sketch.js
@@ -5,6 +5,10 @@ var imgBank = []; // Array for the image prompts
 var wrdBank = []; // Array for the word prompts
 var arr = []; // A test array
 var a, b, c, d; // Random index based on arr length
+var storyImg; // The user's image choice
+var storyCpt; // The user's caption choice
+var q1Clicked, q2Clicked, q3Clicked, q4Clicked = false; // Boolean to register user's clicks
+var stryStage; // A counter to check what level user is at - Go screen (0), Images (1) Captions (2) 
 
 function preload() {
     img1 = loadImage('image-01.jpg');
@@ -51,11 +55,13 @@ function setup() {
      print('Random index a is ' + a.toString());
      print('Random index b is ' + b.toString());
      print('Random index c is ' + c.toString());
-     print('Random index d is ' + d.toString());     
+     print('Random index d is ' + d.toString());
+
+     // Story just beginning - set story stage to 0
+     stryStage = 0;
 }
 
 function draw() {
-    background(50);
     // Get a random image from the array
     var imgA = arr[a];
     var imgB = arr[b];
@@ -63,11 +69,75 @@ function draw() {
     var imgD = arr[d];
 
     
-    // Display the image at (0,0) at half its size
-    image(imgA, 0, 0, imgA.width/4, imgA.height/4);
-    image(imgB, imgA.width/4, 0, imgB.width/4, imgB.height/4);
-    image(imgC, 0, imgB.height/4, imgC.width/4, imgC.height/4);
-    image(imgD, imgC.width/4, imgC.height/4, imgD.width/4, imgD.height/4);
+    // Display the image at the center of each quadrant of the screen. 
+    imageMode(CENTER);
+    image(imgA, width/4, height/4, imgA.width/8, imgA.height/8);
+    image(imgB, (3*width)/4, height/4, imgB.width/8, imgB.height/8);
+    image(imgC, width/4, (3*height)/4, imgC.width/8, imgC.height/8);
+    image(imgD, (3*width)/4, (3*height)/4, imgD.width/8, imgD.height/8);
+
+    // Save the images
+    if(q1Clicked && (stryStage == 0)){
+        background(0);
+        imageMode(CENTER);
+        image(imgA, width/2, height/2, imgA.width/8, imgA.height/8);
+        // Save imgA
+    } else if(q2Clicked && (stryStage == 0)){
+        // Save imgB
+    } else if(q3Clicked && (stryStage == 0)){
+        // Save imgC
+    } else if(q4Clicked && (stryStage == 0)){
+        // Save imgD
+    } 
+    
+    // Save the captions
+    if(q1Clicked && (stryStage == 1)){
+        // Save the cptA
+    } else if(q2Clicked && (stryStage == 1)){
+        // Save the cptB
+    } else if(q3Clicked && (stryStage == 1)){
+        // Save the cptC
+    } else if(q4Clicked && (stryStage == 1)){
+        // Save the cptD
+    }
+
+}
+
+/* An event listener to track which quadrant the user has clicked and set the final image & caption picked by user */
+function mousePressed() {
+        // Get the coordinates where the mouse was clicked
+        var x = mouseX;
+        var y = mouseY;
+        print('Mouse was pressed at (' + mouseX.toString() + ' ,' + mouseY.toString() + ')');
+        // Check which part of the screen we are in
+        if((x < width/2) && (y < height/2)){
+            // First Quadrant Top Left - change q1Clicked
+            q1Clicked = true; 
+            // Set other quadrants to false
+            q2Clicked = false;
+            q3Clicked = false;
+            g4Clicked = false;
+        } else if((x > width/2) && (y < height/2)){
+            // Second Quadrant Top Right - change q2Clicked
+            q2Clicked = true;
+            q2Clicked = false;
+            q3Clicked = false;
+            g4Clicked = false;
+
+        } else if((x < width/2) && (y > height/2)){
+            // Third Quadrant Bottom Left - change q3Clicked
+            q3Clicked = true;
+            q2Clicked = false;
+            q3Clicked = false;
+            g4Clicked = false;
+
+        } else {
+            // Fourth Quadrant Bottom Right - change q4Clicked
+            q4Clicked = true;
+            q2Clicked = false;
+            q3Clicked = false;
+            g4Clicked = false;
+        }
 }
 
 /* Make the canvas responsive -- automatically resize the window */
